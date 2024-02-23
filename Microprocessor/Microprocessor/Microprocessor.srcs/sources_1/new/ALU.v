7159deb52ae8547d0c25aa300a402f8c489d1a55
Michael Wang <1211713891@qq.com>
2024-02-23 15:07:12 +0000

    Revert "merge test"
    This reverts commit ed791780b20ae15689928d7e10cfa8c67d66e688.

--- Microprocessor/Microprocessor/Microprocessor.srcs/sources_1/new/ALU.v
+++ Microprocessor/Microprocessor/Microprocessor.srcs/sources_1/new/ALU.v
@@ -3,34 +3,28 @@ module ALU(
     input [7:0] regB, // 4-bit input operand B
     input [3:0] ALU_Sel, // Operation selector
     output reg [7:0] regO // 4-bit result
-    input shifter_flag,
-    output update
 );
 
 
 // Define operations
-localparam ACC_NZ_A     = 4'b0111;
-localparam ACC_NZ_SH    = 4'b1000;
-localparam ADD          = 4'b1001;
-localparam SUB          = 4'b1010;
-localparam INV          = 4'b1011;
-localparam AND          = 4'b1100;
-localparam OR           = 4'b1101;
-localparam XOR          = 4'b1110;
-
+localparam ADD  = 4'b1001;
+localparam SUB  = 4'b1010;
+localparam AND  = 4'b1100;
+localparam OR   = 4'b1101;
+localparam XOR  = 4'b1110;
+localparam INV  = 4'b1011;
 
 // ALU operation
 always @(ALU_Sel) begin
     case(ALU_Sel)
-        ACC_NZ_A: regO <= regB;
-        ACC_NZ_SH: regO <= regB;
         ADD: regO = regA + regB;
         SUB: regO = regA - regB;
-        INV: regO = ~regA;
         AND: regO = regA & regB;
         OR:  regO = regA | regB;
         XOR: regO = regA ^ regB;
+        INV: regO = ~regA;
     endcase
 end
+
 
 endmodule
